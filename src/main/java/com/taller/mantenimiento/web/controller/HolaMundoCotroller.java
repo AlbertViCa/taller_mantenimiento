ec205e61343d43140db58cfaa47b69ce4b977582
AlbertViCa <albert0-v.c@hotmail.com>
2020-10-28 15:04:54 -0600

    Se estableció la estructura del proyecto.

--- src/main/java/com/taller/mantenimiento/web/controller/HolaMundoCotroller.java
+++ src/main/java/com/taller/mantenimiento/web/controller/HolaMundoCotroller.java
@@ -1,4 +1,4 @@
-package com.taller.mantenimiento;
+package com.taller.mantenimiento.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
