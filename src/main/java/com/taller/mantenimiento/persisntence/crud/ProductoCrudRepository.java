8a25028bbcce822642d15cc20fcee0ec4ae1f958
AlbertViCa <albert0-v.c@hotmail.com>
2020-10-29 17:22:35 -0600

    Se implementó la notación @Repository.

--- src/main/java/com/taller/mantenimiento/persisntence/crud/ProductoCrudRepository.java
+++ src/main/java/com/taller/mantenimiento/persisntence/crud/ProductoCrudRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+
+//Crear QueryMethods
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
     List<Producto> findByIdCategoriaOrderByMarcaAsc(int idCaregoria);
 }
--- src/main/java/com/taller/mantenimiento/persisntence/dao/ProductoRepository.java
+++ src/main/java/com/taller/mantenimiento/persisntence/dao/ProductoRepository.java
@@ -2,9 +2,13 @@ package com.taller.mantenimiento.persisntence.dao;
 
 import com.taller.mantenimiento.persisntence.crud.ProductoCrudRepository;
 import com.taller.mantenimiento.persisntence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -14,5 +18,17 @@ public class ProductoRepository {
 
     public List<Producto> getByCategoria(int idCategoria){
         return productoCrudRepository.findByIdCategoriaOrderByMarcaAsc(idCategoria);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
