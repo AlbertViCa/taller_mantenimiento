7362f971fc5209ea498387cd596bb0c6bab9be34
AlbertViCa <albert0-v.c@hotmail.com>
2020-10-29 17:13:29 -0600

    Se agregaron QueryMethods.

--- src/main/java/com/taller/mantenimiento/persisntence/crud/ProductoCrudRepository.java
+++ src/main/java/com/taller/mantenimiento/persisntence/crud/ProductoCrudRepository.java
@@ -1,9 +1,10 @@
 package com.taller.mantenimiento.persisntence.crud;
 
-import com.taller.mantenimiento.persisntence.entity.Mantenimiento;
 import com.taller.mantenimiento.persisntence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByMarcaAsc(int idCaregoria);
 }
