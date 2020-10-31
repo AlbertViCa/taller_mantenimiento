6d28b98a9631b2dd12299786b33d71f488cfe597
AlbertViCa <albert0-v.c@hotmail.com>
2020-10-30 21:16:54 -0600

    Mejora de respuestas HTTP.
    Se implementó ResponseEntinty para mejorar las respuestas de los controladores, también se implementó HttpStatus para retornar un código según el caso de respuesta.

--- src/main/java/com/taller/mantenimiento/web/controller/ProductController.java
+++ src/main/java/com/taller/mantenimiento/web/controller/ProductController.java
@@ -3,10 +3,11 @@ package com.taller.mantenimiento.web.controller;
 import com.taller.mantenimiento.business.domain.Product;
 import com.taller.mantenimiento.business.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -16,27 +17,35 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
-    public List<Product> getAll(){
-        return productService.getAll();
+    public ResponseEntity<List<Product>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Optional<Product> getProduct(@PathVariable("id") int productId){
-        return productService.getProduct(productId);
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int catecoryId){
-        return productService.getByCategory(catecoryId);
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int catecoryId){
+        return productService.getByCategory(catecoryId)
+                .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/save")
-    public Product save(Product product){
-        return productService.save(product);
+    public ResponseEntity<Product> save(Product product){
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
-    public boolean delete(@PathVariable("id") int productId){
-        return productService.delete(productId);
+    public ResponseEntity delete(@PathVariable("id") int productId){
+        if(productService.delete(productId)){
+            return new ResponseEntity(HttpStatus.OK);
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
