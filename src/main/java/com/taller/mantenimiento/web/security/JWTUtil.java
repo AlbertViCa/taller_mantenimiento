3484bd0820f71355368e94fd9e13f6ce9478fddf
AlbertViCa <albert0-v.c@hotmail.com>
2020-11-01 19:21:32 -0600

    Se configuró la genearción de un JWT.

---
+++ src/main/java/com/taller/mantenimiento/web/security/JWTUtil.java
@@ -0,0 +1,18 @@
+package com.taller.mantenimiento.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JWTUtil {
+
+    private static final String KEY = "a8-ry-a9r8huds0-435qasre";
+
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 100 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
+}
